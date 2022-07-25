8b5b590ac07b98b3385cde1a9fb62832de08c06c
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-07-25 20:09:48 +0200

    chore: landing page

--- pages/geist.tsx
+++ pages/geist.tsx
@@ -1,9 +1,12 @@
 import React from "react"
 import { NextPage } from "next"
-import { useTheme, Grid } from "@geist-ui/core"
+import { useTheme, Grid, Avatar, Button, Spacer } from "@geist-ui/core"
 import PackageIcon from "@geist-ui/icons/package"
 import FeatherIcon from "@geist-ui/icons/feather"
 import GitHubIcon from "@geist-ui/icons/github"
+import HomeCell from "../components/HomeCell"
+import Image from "next/image"
+import { Display, Key, User } from "@geist-ui/icons"
 
 const Application: NextPage<{}> = () => {
   const theme = useTheme()
@@ -11,55 +14,59 @@ const Application: NextPage<{}> = () => {
   return (
     <>
       <div className="layout">
+        <div className="image">
+          <Image src="/logo.jpg" width="240px" height="280px" />
+        </div>
         <div className="hero">
-          <h1 className="title">Geist</h1>
-          <h3 className="desc">
-            An open source design system for building`` modern websites and
-            applications.
-          </h3>
+          <h1 className="title">Votex</h1>
+          <h3 className="desc">An online voting system for UMaT students.</h3>
         </div>
 
         <Grid.Container gap={2} justify="center">
-          {/* <Grid xs={24} md={8}>
+          <Grid xs={24} md={10}>
             <HomeCell
-              icon={<PackageIcon />}
-              url="/en-us/components"
-              title="Components"
-              desc="Ever-increasing list of concise and aesthetic components."
+              icon={<Key />}
+              title="Secure"
+              desc="Securely store your votes and vote on your own behalf."
             />
           </Grid>
-          <Grid xs={24} md={8}>
+          <Grid xs={24} md={10}>
             <HomeCell
-              icon={<FeatherIcon />}
-              url="/en-us/guide/themes"
-              title="Customizable"
-              desc="Configure sizes, colors, appearances, shapes, and more."
+              icon={<Display />}
+              title="Portable"
+              desc="Vote at your own convinience anytime, anywhere, with any device."
             />
           </Grid>
-          <Grid xs={24} md={8}>
-            <HomeCell
-              icon={<GitHubIcon />}
-              url="https://github.com/geist-org/geist-ui"
-              title="Open Sourced"
-              desc="Geist is open sourced and available free under MIT licence."
-            />
-          </Grid> */}
         </Grid.Container>
+        <Spacer h={4} />
+
+        <div className="image">
+          <Button auto scale={2} icon={<User />} type="default">
+            Sign In
+          </Button>
+        </div>
       </div>
       <style jsx>{`
         .layout {
           min-height: calc(100vh - var(--geist-page-nav-height));
           max-width: ${theme.layout.pageWidthWithMargin};
           margin: 0 auto;
-          padding: 0 ${theme.layout.gap} calc(${theme.layout.gap} * 2);
+          padding: 4rem;
           box-sizing: border-box;
         }
         .hero {
           height: calc(100vh - var(--geist-page-nav-height) - 300px);
-          min-height: 30vh;
+          min-height: 15vh;
           max-width: 500px;
           margin: 0 auto;
           text-align: center;
+          align-items: center;
+          justify-content: center;
+          display: flex;
+          flex-direction: column;
+        }
+        .image {
+          margin: 0 auto;
           align-items: center;
           justify-content: center;
           display: flex;
